0229c6360bd3b831c4511509118760235fcb8f3e
Jesper Johansen <kontakt@jayj.dk>
2012-12-05 19:56:39 +0100

    Assign the plugin class to a variable

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * Description: Adds an Youtube username field to the user profile page and allow you to show your videos on your website.
  * Author: Jayjdk
  * Author URI: http://jayj.dk
- * Version: 3.0.0
+ * Version: 3.0.1
  * License: GPLv2 or later
  */
 
@@ -109,6 +109,6 @@ class Youtube_Profile_Field {
 	}
 }
 
-new Youtube_Profile_Field();
+$youtube_profile_field = new Youtube_Profile_Field();
 
 ?>
